e95868dad79e1b58fbff6850c2ad31c8595f3baa
Jakub Rodzik <jakub.rodzik@housecallpro.com>
2022-12-01 09:30:19 +0100

    day1p2

--- day_1/solution.rb
+++ day_1/solution.rb
@@ -16,5 +16,31 @@ def part_1(input)
   [max_calories, current_calories].max
 end
 
+def part_2(input)
+  current_calories = 0
+  top_3 = []
+
+  input.each do |calories|
+    if calories == ''
+      if current_calories > top_3.min.to_i
+        top_3 << current_calories
+        top_3.sort!.shift if top_3.size > 3
+      end
+
+      current_calories = 0
+    else
+      current_calories += calories.to_i
+    end
+  end
+
+  if current_calories > top_3.min.to_i
+    top_3 << current_calories
+    top_3.sort!.shift if top_3.size > 3
+  end
+
+  top_3.sum
+end
+
 input = File.read('day_1/input.txt').split("\n")
 puts part_1(input)
+puts part_2(input)
--- day_1/solution_spec.rb
+++ day_1/solution_spec.rb
@@ -1,9 +1,18 @@
 require 'rspec'
 require_relative 'solution'
 
-describe 'Part 1' do
-  it 'finds the elf carrying the most calories' do
-    input = File.read('day_1/test_input.txt').split("\n")
-    expect(part_1(input)).to eq(24000)
+describe 'Day 1' do
+  describe 'part 1' do
+    it 'finds the the most calories carried by an elf' do
+      input = File.read('day_1/test_input.txt').split("\n")
+      expect(part_1(input)).to eq(24000)
+    end
+  end
+
+  describe 'part 2' do
+    it 'finds sum of calories carried by 3 elves' do
+      input = File.read('day_1/test_input.txt').split("\n")
+      expect(part_2(input)).to eq(45000)
+    end
   end
 end

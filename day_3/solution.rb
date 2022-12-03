a44cad56badf65b4b2e6431c977b2aa8e38a4d2f
Jakub Rodzik <jakub.rodzik@housecallpro.com>
2022-12-03 09:31:08 +0100

    day3p2

--- day_3/solution.rb
+++ day_3/solution.rb
@@ -11,5 +11,18 @@ def part_1(input)
   score
 end
 
+def part_2(input)
+  chars = ('a'..'z').to_a + ('A'..'Z').to_a
+  score = 0
+
+  input.each_slice(3) do |lines|
+    element = lines.map { |s| s.split('') }.reduce(:&)[0]
+    score += chars.index(element) + 1
+  end
+
+  score
+end
+
 input = File.read('day_3/input.txt').split("\n")
 puts part_1(input)
+puts part_2(input)
--- day_3/solution_spec.rb
+++ day_3/solution_spec.rb
@@ -3,9 +3,16 @@ require_relative 'solution'
 
 describe 'Day 3' do
   describe 'part 1' do
-    it 'counts points' do
+    it 'sums char values' do
       input = File.read('day_3/test_input.txt').split("\n")
       expect(part_1(input)).to eq(157)
+    end
+  end
+
+  describe 'part 2' do
+    it 'sums char values of each group' do
+      input = File.read('day_3/test_input.txt').split("\n")
+      expect(part_2(input)).to eq(70)
     end
   end
 end

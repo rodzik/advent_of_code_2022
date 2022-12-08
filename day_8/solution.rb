9d6806af08175a064cb39af9fd566a5270afd926
Jakub Rodzik <jakub.rodzik@housecallpro.com>
2022-12-08 09:29:22 +0100

    day8p2

--- day_8/solution.rb
+++ day_8/solution.rb
@@ -17,5 +17,51 @@ def part_1(input)
   visible_trees.uniq.size
 end
 
+def part_2(input)
+  trees = input.map { |row| row.split('').map(&:to_i) }
+  max_score = 0
+
+  trees.each_with_index do |line, y|
+    line.each_with_index do |tree, x|
+      next if x == 0 || x == line.size - 1 || y == 0 || y == trees.size - 1
+
+      scores = Hash.new(0)
+      i = 1
+      while(x+i < line.size)
+        scores[:right] += 1
+        break if trees[y][x+i] >= tree
+        i += 1
+      end
+
+      i = 1
+      while(x-i >= 0)
+        scores[:left] += 1
+        break if trees[y][x-i] >= tree
+        i += 1
+      end
+
+      i = 1
+      while(y+i < trees.size)
+        scores[:down] += 1
+        break if trees[y+i][x] >= tree
+        i += 1
+      end
+
+      i = 1
+      while(y-i >= 0)
+        scores[:up] += 1
+        break if trees[y-i][x] >= tree
+        i += 1
+      end
+
+      score = scores.values.reduce(:*)
+      max_score = score if score > max_score
+    end
+  end
+
+  max_score
+end
+
 input = File.read('day_8/input.txt').split("\n")
 puts part_1(input)
+puts part_2(input)
--- day_8/solution_spec.rb
+++ day_8/solution_spec.rb
@@ -3,9 +3,16 @@ require_relative 'solution'
 
 describe 'Day 8', :aggregate_failures do
   describe 'part 1' do
-    it 'sums sizes markers' do
+    it 'find trees' do
       input = File.read('day_8/test_input.txt').split("\n")
       expect(part_1(input)).to eq(21)
+    end
+  end
+
+  describe 'part 2' do
+    it 'finds best view' do
+      input = File.read('day_8/test_input.txt').split("\n")
+      expect(part_2(input)).to eq(8)
     end
   end
 end

7e4a81c8627ba9d254915a0605ad5294340a6498
Jakub Rodzik <jakub.rodzik@housecallpro.com>
2022-12-10 18:03:39 +0100

    day9p2

--- day_9/solution.rb
+++ day_9/solution.rb
@@ -1,68 +1,95 @@
-class Line
+class Knot
+  attr_reader :x, :y
+
   def initialize
-    @hx = 0
-    @hy = 0
-    @tx = 0
-    @ty = 0
-    @tail_positions = [[0,0]]
+    @x = 0
+    @y = 0
+    @positions = [[0, 0]]
+  end
+
+  def move(dir)
+    case dir
+    when 'R' then @x += 1
+    when 'L' then @x -= 1
+    when 'U' then @y += 1
+    when 'D' then @y -= 1
+    end
+  end
+
+  def align_with_knot(head)
+    return unless alignment_needed?(head)
+
+    align(head)
+  end
+
+  def positions_count
+    @positions.uniq.size
+  end
+
+  private
+
+  def alignment_needed?(head)
+    !((head.x - 1..head.x + 1).include?(x) &&  (head.y - 1..head.y + 1).include?(y))
+  end
+
+  def align(head)
+    if head.x != x && head.y != y
+      move_x(head)
+      move_y(head)
+    elsif (head.x - x).abs > 1
+      move_x(head)
+    elsif (head.y - y).abs > 1
+      move_y(head)
+    end
+
+    @positions << [x, y]
+  end
+
+  def move_x(head)
+    @x += 1 if head.x > x
+    @x -= 1 if head.x < x
+  end
+
+  def move_y(head)
+    @y += 1 if head.y > y
+    @y -= 1 if head.y < y
+  end
+end
+
+class Line
+  def initialize(knots_count)
+    @knots = Array.new(knots_count) { Knot.new }
   end
 
   def move(cmd)
     dir = cmd.split(' ')[0]
     amount = cmd.split(' ')[1].to_i
-    amount.times { |_| move_head(dir) }
+    amount.times { |_| move_knots(dir) }
   end
 
   def tail_positions_count
-    @tail_positions.uniq.size
+    @knots.last.positions_count
   end
 
   private
 
-  def move_head(dir)
-    case dir
-    when 'R' then @hx += 1
-    when 'L' then @hx -= 1
-    when 'U' then @hy += 1
-    when 'D' then @hy -= 1
+  def move_knots(dir)
+    @knots.each_with_index do |knot, i|
+      if i == 0
+        knot.move(dir)
+      else
+        knot.align_with_knot(@knots[i - 1])
+      end
     end
-
-    move_tail if tail_move_needed?
-  end
-
-  def tail_move_needed?
-    !((@hx - 1..@hx + 1).include?(@tx) &&  (@hy - 1..@hy + 1).include?(@ty))
-  end
-
-  def move_tail
-    if @hx != @tx && @hy != @ty
-      move_tail_x
-      move_tail_y
-    elsif (@hx - @tx).abs > 1
-      move_tail_x
-    elsif (@hy - @ty).abs > 1
-      move_tail_y
-    end
-
-    @tail_positions << [@tx, @ty]
-  end
-
-  def move_tail_x
-    @tx += 1 if @hx > @tx
-    @tx -= 1 if @hx < @tx
-  end
-
-  def move_tail_y
-    @ty += 1 if @hy > @ty
-    @ty -= 1 if @hy < @ty
   end
 end
 
-def part_1(input)
-  line = Line.new
+def solution(input, knots_count)
+  line = Line.new(knots_count)
   input.each { |cmd| line.move(cmd) }
   line.tail_positions_count
 end
 
 input = File.read('day_9/input.txt').split("\n")
-puts part_1(input)
+puts solution(input, 2)
+puts solution(input, 10)
--- day_9/solution_spec.rb
+++ day_9/solution_spec.rb
@@ -3,9 +3,16 @@ require_relative 'solution'
 
 describe 'Day 9', :aggregate_failures do
   describe 'part 1' do
-    it 'find signal strength' do
+    it 'finds tail positions' do
       input = File.read('day_9/test_input.txt').split("\n")
-      expect(part_1(input)).to eq(13)
+      expect(solution(input, 2)).to eq(13)
+    end
+  end
+
+  describe 'part 1' do
+    it 'finds tails positions' do
+      input = File.read('day_9/test_input_2.txt').split("\n")
+      expect(solution(input, 10)).to eq(36)
     end
   end
 end
